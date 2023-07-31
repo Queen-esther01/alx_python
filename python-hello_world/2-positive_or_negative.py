b29f4810eef4dd6fc36af522aead622b5ccd7126
Esther <ejidikeesther@gmail.com>
2023-07-31 22:36:14 +0100

    Convert number to string

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,9 +1,8 @@
 import random
 number = random.randint(-10, 10)
 if(number > 0):
-    print(number + ' is positive')
+    print(str(number) + ' is positive')
 elif(number == 0):
-    print(number + ' is zero')
+    print(str(number) + ' is zero')
 elif(number < 0):
-    print(number + ' is negative')
-    
+    print(str(number) + ' is negative')
