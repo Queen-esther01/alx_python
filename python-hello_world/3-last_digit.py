af9c553fb2677979f12ef5e14c28fc60d2be727c
Esther <ejidikeesther@gmail.com>
2023-07-31 22:56:16 +0100

    Refactor

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,12 +1,12 @@
 import random
 number = random.randint(-10000, 10000)
 test = 'hello'
-last = str(number)[-1]
+last = int(str(number)[-1])
 message = 0
-if((int(last)) > 5):
+if(last > 5):
     message = 'and is greater than 5'
-elif((int(last)) == 0):
+elif(last == 0):
     message  = 'and is 0'
-elif((int(last)) < 6 and (int(last)) > 0):
+elif(last < 6 and last > 0):
     message  = 'and is less than 6 and not 0'
 print(f'Last digit of {number} is {last} {message}')
